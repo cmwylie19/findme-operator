1a5b057a91501a83ad3efe3de8749f9b45d7ca1d
Casey Wylie <cmwylie19@gmail.com>
2021-08-30 08:27:22 -0400

    updates

--- operator/controllers/suite_test.go
+++ operator/controllers/suite_test.go
@@ -41,6 +41,55 @@ var cfg *rest.Config
 var k8sClient client.Client
 var testEnv *envtest.Environment
 
+// type Book struct {
+// 	Title  string
+// 	Author string
+// 	Pages  uint32
+// }
+
+// func (b *Book) CategoryByLength() string {
+// 	if b.Pages > 300 {
+// 		return "NOVEL"
+// 	} else {
+// 		return "SHORT STORY"
+// 	}
+// }
+
+// var _ = Describe("Book", func() {
+// 	var (
+// 		longBook  Book
+// 		shortBook Book
+// 	)
+
+// 	BeforeEach(func() {
+// 		longBook = Book{
+// 			Title:  "Les Miserables",
+// 			Author: "Victor Hugo",
+// 			Pages:  2783,
+// 		}
+
+// 		shortBook = Book{
+// 			Title:  "Fox In Socks",
+// 			Author: "Dr. Seuss",
+// 			Pages:  24,
+// 		}
+// 	})
+
+// 	Describe("Categorizing book length", func() {
+// 		Context("With more than 300 pages", func() {
+// 			It("should be a novel", func() {
+// 				Expect(longBook.CategoryByLength()).To(Equal("NOVEL"))
+// 			})
+// 		})
+
+// 		Context("With fewer than 300 pages", func() {
+// 			It("should be a short story", func() {
+// 				Expect(shortBook.CategoryByLength()).To(Equal("SHORT STORY"))
+// 			})
+// 		})
+// 	})
+// })
+
 func TestAPIs(t *testing.T) {
 	RegisterFailHandler(Fail)
 
